4b0b4989b66887b2f300d65c6f9899d4c21cf0e7
xevolesi <easytomalbolge@gmail.com>
2024-10-31 14:36:33 +0300

    add alexnet model

--- source/models/__init__.py
+++ source/models/__init__.py
@@ -1,3 +1,3 @@
-from .alexnet import AlexNet
+from .alexnet import PAPER_ERROR_RATE_AT_1, PAPER_ERROR_RATE_AT_5, AlexNet
 
-__all__ = ["AlexNet"]
+__all__ = ["AlexNet", "PAPER_ERROR_RATE_AT_1", "PAPER_ERROR_RATE_AT_5"]
